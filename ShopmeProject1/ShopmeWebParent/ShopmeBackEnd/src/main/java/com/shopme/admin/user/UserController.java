7a299c0bd33a554e6145035e0de187eb5e43c68d
dheeru177 <dheeru177@gmail.com>
2023-05-30 15:26:51 +0530

    Lec 42 Code Refactor

--- ShopmeProject1/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeProject1/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.util.StringUtils;
 
 import com.shopme.admin.FileUploadUtil;
 import com.shopme.common.entity.Role;
@@ -27,12 +29,56 @@ public class UserController {
 	private UserService service;
 
 	@GetMapping("/users")
-	public String listAll(Model model) {
+	public String listFirstPage(Model model) {
 
-		List<User> listUsers = service.listAll();
-		model.addAttribute("listUsers", listUsers);
-		return "users";
+	return listByPage(1, model, "firstName" , "asc" , null);
 	}
+	
+	
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(
+			@PathVariable(name = "pageNum")int pageNum , Model model,
+			
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
+			@Param("keyword") String keyword
+			) {
+		System.out.println("Sort Field: " + sortField);
+		System.out.println("Sort Order: " + sortDir);
+		
+		Page<User> page = service.listByPage(pageNum , sortField , sortDir , keyword);
+		List<User> listUsers = page.getContent();
+		
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
+		if(endCount > page.getTotalElements())
+		{
+			
+		endCount = 	page.getTotalElements();
+			
+			
+		}
+		
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		
+
+		
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems",page.getTotalElements());
+		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);
+		return "users";
+		
+	}
+	
+	
+	
 
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
@@ -74,14 +120,21 @@ public class UserController {
 		{
 			
 			user.setPhotos(null);
-			service.save(user);
-		}
 			
+		}
+		service.save(user);
 		}
 		
 		//service.save(user);
 		redirectAttributes.addFlashAttribute("message" , "The user has been saved successfully ");
-		return "redirect:/users";
+		
+		return getRedirectURLtoAffectedUser(user);
+	}
+
+
+	private String getRedirectURLtoAffectedUser(User user) {
+		String firstPartOfEmail = user.getEmail().split("@")[0]; 
+		return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
 	}
 
 	@GetMapping("/users/edit/{id}")
