cf6282fb4c003e944e647f75c73fd18910f3b2ac
dheeru177 <dheeru177@gmail.com>
2023-05-31 22:00:23 +0530

    Enable remember me for login

--- ShopmeProject1/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject1/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -65,7 +65,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	                                          .loginPage("/login")
 	                                          .usernameParameter("email")
 		                                      .permitAll()
-	                                       .and().logout().permitAll();
+	                                       .and().logout().permitAll()
+	                                       .and().rememberMe()
+	                                       .key("AbcDefgHjklmnOpqrs_1234567890")
+	                                       .tokenValiditySeconds(7 * 24 * 60 * 60);
 		
 		
 	}
